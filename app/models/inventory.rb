1d43de02b91eab8185a42bb01c4db57291c23aa9
J. Paterson <contact@jpatersondev.com>
2020-01-27 19:30:17 -0800

    update associations for inventory and order

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,2 +1,5 @@
 class Inventory < ApplicationRecord
+  has_many :inventory_orders
+  has_many :orders, through: :inventory_orders
+  has_many :supplies
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,4 @@
 class Order < ApplicationRecord
+  has_many :inventory_orders
+  has_many :inventories, through: :inventory_orders
 end
