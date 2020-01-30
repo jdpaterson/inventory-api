67a98e1d4f6049853dc949b0dceae7241dd2e0fe
J. Paterson <contact@jpatersondev.com>
2020-01-29 21:53:24 -0800

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,56 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+inventories = (1..5).map do
+  {
+    name: Faker::Cannabis.brand,
+    description: "#{Faker::Cannabis.cannabinoid} | #{Faker::Cannabis.type} | #{Faker::Cannabis.health_benefit}",
+    price: Faker::Number.decimal(l_digits: 2),
+    supplies_attributes: [
+      {
+        supplied_amount: 100
+      }
+    ]
+  }
+end
+Inventory.create(inventories)
+
+orders = [
+  {
+    customer_email_address: Faker::Internet.email,
+    date_order_placed: Faker::Date.in_date_period,
+    inventory_orders_attributes: [
+      {
+        inventory: Inventory.first,
+        order_quantity: 25
+      },
+      {
+        inventory: Inventory.second,
+        order_quantity: 50
+      },
+      {
+        inventory: Inventory.third,
+        order_quantity: 75
+      }
+    ]
+  },
+  {
+    customer_email_address: Faker::Internet.email,
+    date_order_placed: Faker::Date.in_date_period,
+    inventory_orders_attributes: [
+      {
+        inventory: Inventory.first,
+        order_quantity: 30
+      },
+      {
+        inventory: Inventory.second,
+        order_quantity: 25
+      },
+      {
+        inventory: Inventory.third,
+        order_quantity: 10
+      }
+    ]
+  }
+]
+
+Order.create(orders)
